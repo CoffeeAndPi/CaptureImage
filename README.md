c36b68be2eba6b55f3ea0367f5691d3e0d61bc2b
Brad <brad@bizzartech.com>
2018-07-04 10:43:24 -0400

    update readme, capture script

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # CaptureImage
 Use Raspberry Pi Zero W and Camera Module to take pictures and save to AWS S3 bucket.
 
+# Install requirements
+* sudo apt-get update && sudo apt-get upgrade
+* sudo apt-get install imagemagick
+* sudo pip install awscli
+* aws configure
+
 # Add to crontab
 */2 * * * * sh /home/pi/scripts/capture-image.sh > /dev/null 2>&1
--- capture-image.sh
+++ capture-image.sh
@@ -6,14 +6,14 @@ DAY=`date "+%m-%d-%Y"`
 TIME=`date "+%H-%M"`
 
 raspistill -o $FILE
-convert $FILE -font arial.ttf \
+convert $FILE -font Liberation-Sans \
 	-pointsize 72 -fill white \
 	-annotate +100+100 "Raspberry Pi Cam by BradDerstine.com" \
 	-annotate +100+200 "$DATETIMESTAMP | Philadelphia, PA, USA" \
 	$NEWFILE
 
-/home/pi/.local/bin/aws s3 cp $NEWFILE s3://bradderstine-pi-cam/current.jpg
-/home/pi/.local/bin/aws s3 cp $NEWFILE s3://bradderstine-pi-cam/$DAY/$TIME.jpg
+/usr/local/bin/aws s3 cp $NEWFILE s3://bradderstine-pi-cam/current.jpg
+/usr/local/bin/aws s3 cp $NEWFILE s3://bradderstine-pi-cam/$DAY/$TIME.jpg
 
 rm $FILE
 rm $NEWFILE
